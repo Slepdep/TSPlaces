dbf2420ce43bcf1c1e4ff55778e28988b0436d86
Dakota <omailuz@gmail.com>
2019-10-12 05:51:30 -0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { FavoritePlacesComponent } from './favorite-places/favorite-places.component';
+
+import { PlacesService } from './services/places.service';
+import { PlaceFormComponent } from './place-form/place-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlaceFormComponent,
+    FavoritePlacesComponent;
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule;
+  ],
+  providers: [PlacesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
